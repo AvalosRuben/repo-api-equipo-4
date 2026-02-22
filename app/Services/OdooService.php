bd3158603146f96af6fd3f45db18ca3b84dba88d
AvalosRuben <rubenavalosmtz@gmail.com>
2026-02-21 21:43:49 -0600

    Get endpoints for products
    By id and by bunches of 10

--- app/Services/OdooService.php
+++ app/Services/OdooService.php
@@ -1,8 +1,11 @@
+<?php
+
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
 
-class OdooService{
+class OdooService
+{
     protected string $url;
     protected string $db;
     protected string $username;
@@ -17,23 +20,23 @@ class OdooService{
         $this->password = config('services.odoo.password');
     }
 
-    protected function callRpc(string $service, string $method, array $args=[])
+    protected function callRpc(string $service, string $method, array $args = []): array
     {
-        $response = Http::post($this->url,[
-            'jsonrpc'=>'2.0',
-            'method'=> 'call',
-            'params'=>[
-                'service'=>$service,
-                'method'=>$method,
-                'args'=> $args,
-                ],
-                'id'=>1,
-                ]);
-                return $response->json();
+        $response = Http::post($this->url, [
+            'jsonrpc' => '2.0',
+            'method'  => 'call',
+            'params'  => [
+                'service' => $service,
+                'method'  => $method,
+                'args'    => $args,
+            ],
+            'id' => 1,
+        ]);
+
+        return $response->json();
     }
 
-    /*Login to get UID */
-     protected function login(): int
+    protected function login(): int
     {
         if ($this->uid !== null) {
             return $this->uid;
@@ -80,6 +83,38 @@ class OdooService{
         return $response['result'];
     }
 
+
+    public function getProducts(int $limit = 10, int $offset = 0): array
+    {
+        return $this->executeKw(
+            'product.template',
+            'search_read',
+            [
+                []
+            ],
+            [
+                'fields' => ['id', 'name', 'list_price'],
+                'limit'  => $limit,
+                'offset' => $offset,
+                'order'  => 'id asc',
+            ]
+        );
+    }
+
+    public function getProductById(int $id): array
+    {
+        $result = $this->executeKw(
+            'product.template',
+            'search_read',
+            [
+                [['id', '=', $id]]
+            ],
+            [
+                'fields' => ['id', 'name', 'list_price'],
+                'limit'  => 1,
+            ]
+        );
+
+        return $result[0] ?? [];
+    }
 }
-
-
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Services\OdooService;
+
+
+// GET  - 10 en 10
+Route::get('/productos', function (OdooService $odoo) {
+    $limit = (int) request()->get('limit', 10);
+    $offset = (int) request()->get('offset', 0);
+
+    return response()->json(
+        $odoo->getProducts($limit, $offset)
+    );
+});
+
+// GET por id indv
+Route::get('/productos/{id}', function ($id, OdooService $odoo) {
+    $product = $odoo->getProductById((int) $id);
+
+    if (empty($product)) {
+        return response()->json([
+            'message' => 'Producto no encontrado'
+        ], 404);
+    }
+
+    return response()->json($product);
+});
